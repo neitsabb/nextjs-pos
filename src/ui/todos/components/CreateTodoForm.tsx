a243b9ded1ed5cc0710e3941157f6e8bb5ae1e8b
BastienVdp <bastienpp@hotmail.com>
2024-07-06 17:30:18 +0200

    fix: reset fields when success

--- src/ui/todos/components/CreateTodoForm.tsx
+++ src/ui/todos/components/CreateTodoForm.tsx
@@ -5,15 +5,21 @@ import {
   CreateTodoSchema,
 } from "@/application/schemas/todo.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRef } from "react";
 import { Form, useForm } from "react-hook-form";
 import { useServerAction } from "zsa-react";
 
 export const CreateTodoForm = ({ createTodo }: { createTodo: any }) => {
-  const { isError, isPending, executeFormAction, error } =
+  const formRef = useRef<HTMLFormElement>(null);
+  const { isError, isPending, isSuccess, executeFormAction, error } =
     useServerAction(createTodo);
 
+  if (isSuccess && formRef.current) {
+    formRef.current.reset();
+  }
+
   return (
-    <form action={executeFormAction}>
+    <form action={executeFormAction} ref={formRef}>
       <input
         className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
         name="title"
