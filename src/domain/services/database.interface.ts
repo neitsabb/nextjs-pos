304d717ce362c8224dd62c241673f9960b124408
BastienVdp <bastienpp@hotmail.com>
2024-07-06 15:15:05 +0200

    feat: create adapter for supabase

---
+++ src/domain/services/database.interface.ts
@@ -0,0 +1,7 @@
+export interface IDatabaseAdapter {
+  all<T>(table: string): Promise<T[]>;
+  find<T>(table: string, id: string): Promise<T>;
+  update<T>(table: string, id: string, data: T): Promise<T>;
+  create<T>(table: string, data: T): Promise<T>;
+  delete<T>(table: string, id: string): Promise<void>;
+}
---
+++ src/infrastructure/adapters/supabase.adapter.ts
@@ -0,0 +1,36 @@
+import {
+  SupabaseClient,
+  SupabaseClientType,
+} from "./../services/supabase.service";
+import { IDatabaseAdapter } from "@/domain/services/database.interface";
+
+export const SupabaseAdapter = (db: SupabaseClientType): IDatabaseAdapter => ({
+  async all<T>(table: string): Promise<T[]> {
+    const { data, error } = await db.from(table).select("*");
+    if (error) throw new Error(error.message);
+    return data || [];
+  },
+  async find<T>(table: string, id: string): Promise<T> {
+    const { data, error } = await db
+      .from(table)
+      .select("*")
+      .eq("id", id)
+      .single();
+    if (error) throw new Error(error.message);
+    return data || {};
+  },
+  async update<T>(table: string, id: string, data: T): Promise<T> {
+    const { error } = await db.from(table).update(data).eq("id", id);
+    if (error) throw new Error(error.message);
+    return data;
+  },
+  async create<T>(table: string, data: T): Promise<T> {
+    const { data: response, error } = await db.from(table).insert(data);
+    if (error) throw new Error(error.message);
+    return response ? response[0] : data;
+  },
+  async delete<T>(table: string, id: string): Promise<void> {
+    const { error } = await db.from(table).delete().eq("id", id);
+    if (error) throw new Error(error.message);
+  },
+});
